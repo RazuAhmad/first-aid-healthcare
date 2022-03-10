f5fd274dc50815d668d4e8b9b9c81e3f39a3d10d
RazusWebsite <abdurrahmanrazu2000@gmail.com>
2022-03-10 17:55:40 +0600

    navbar done

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Carousel from "./Components/Carousel/Carousel";
+import Navbar from "./Components/Navbar/Navbar";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <br />
+      <br />
+      <Carousel />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "./Navbar.css";
+import logo from "./logo.png";
+import { Link } from "react-router-dom";
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="navLogo">
+        <img src={logo} alt="" />
+      </div>
+
+      <div>
+        <ul className="navLink">
+          <li>
+            <Link style={{ textDecoration: "none" }} to="/home">
+              Home
+            </Link>{" "}
+          </li>
+          <li>
+            <Link style={{ textDecoration: "none" }} to="/ourServices">
+              Our Services
+            </Link>
+          </li>
+          <li>
+            <Link style={{ textDecoration: "none" }} to="/aboutUs">
+              About Us
+            </Link>
+          </li>
+          <li>
+            <Link style={{ textDecoration: "none" }} to="/signIn">
+              Sign in
+            </Link>
+          </li>
+          <li>
+            <Link style={{ textDecoration: "none" }} to="/appointment">
+              Appointment
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
